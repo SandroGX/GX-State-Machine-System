e9b8cfb0780681376843de86f15a4772fc734b83
SandroGX <sandroalemflor@hotmail.com>
2019-09-21 22:16:02 +0100

    Created package.

--- Runtime/StateMachineController.cs
+++ Runtime/StateMachineController.cs
@@ -29,23 +29,4 @@ namespace GX.StateMachineSystem
             Client.StopSM();
         }
     }
-
-    public class VarComponent : Variable
-    {
-        public System.Type compType;
-
-        public override Variable Duplicate(object varHolder)
-        {
-            if (isStatic)
-            {
-                if(va == null)
-
-                return this;
-            }
-
-            VarComponent v = CreateInstance<VarComponent>();
-            v.va = ((Component)varHolder).GetComponent(compType);
-            return v;
-        }
-    }
 }
